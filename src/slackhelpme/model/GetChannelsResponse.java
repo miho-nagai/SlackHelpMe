c8316c3ee903e311b17b8f54444195ff77c35519
miho-nagai <miho.nagai@opentone.co.jp>
2015-11-24 17:21:17 +0900

    gitignoreの文字コード修正

--- src/slackhelpme/model/GetChannelsResponse.java
+++ src/slackhelpme/model/GetChannelsResponse.java
@@ -1,20 +1,20 @@
 package slackhelpme.model;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 // channels.listのレスポンスJSONを表すオブジェクト
 public class GetChannelsResponse extends SlackResponse {
 
-    private ArrayList<Channel> channels;
+    private List<Channel> channels;
 
-    public ArrayList<Channel> getChannels() {
+    public List<Channel> getChannels() {
         // チャンネル名順にソート
         Collections.sort(channels, (c1, c2) -> c1.getName().compareTo(c2.getName()));
         return channels;
     }
 
-    public void setChannels(ArrayList<Channel> channels) {
+    public void setChannels(List<Channel> channels) {
         this.channels = channels;
     }
 }
